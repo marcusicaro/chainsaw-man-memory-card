c87b81378136abb5bd59c6b248db4a0e62b005d4
Marcus <marcusicaromc@gmail.com>
2022-12-27 10:00:18 -0300

    delete unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Main from './components/Main';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,98 +1,88 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import uniqid from 'uniqid';
 
 export default function Cards(props) {
-    const [state, setState] = useState([
+    const getInitialShuffle = () => shuffleArray([
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/2/23/Power.png',
             name: 'Power',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/9/91/Himeno.png',
             name: 'Himeno',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/6/69/Kobeni.png',
             name: 'Kobeni',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/c/c9/Makima_Control.png',
             name: 'Makima',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/0/09/Reze.png',
             name: 'Reze',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/c/c6/Quanxi.png',
             name: 'Quanxi',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/0/0f/Sawatari.png',
             name: 'Akane',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/4/43/Asa_Mitaka.png',
             name: 'Asa',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/b/b5/Yuko.png',
             name: 'Yuko',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
         {
             image: 'https://static.wikia.nocookie.net/chainsaw-man/images/3/39/War_Devil_Face.png',
             name: 'Yoru',
-            status: 'not clicked',
+            clicked: false,
             id: uniqid(),
         },
-    ]);
-      
-    useEffect(() => {
-        const stateCopy = [...state]
-        shuffleArray(stateCopy);
-        setState(stateCopy);
-    }, [])
+    ])
+    const [cards, setCards] = useState(getInitialShuffle);
 
-    const cardClick = (card) => {
-        const stateCopy = [...state];
+    const handleCardClick = (card) => {
+        const cardsCopy = [...cards];
 
-        function handleShuffle() {
-            shuffleArray(stateCopy);
-            setState(stateCopy);
-        }
-
-        if (card.status === 'not clicked') {
-            stateCopy.map(el => {if (el.id === card.id) {el.status = 'clicked'}})
-            handleShuffle();
+        if (!card.clicked) {
+            cardsCopy.forEach(el => {(el.id === card.id) && (el.clicked = !el.clicked)})
+            setCards(() => shuffleArray(cardsCopy))
             return props.handleIncreaseScore();
         }
 
-        stateCopy.map(el => {el.status = 'not clicked';})
-        handleShuffle();
+        cardsCopy.forEach(el => {el.clicked = false})
+        setCards(getInitialShuffle)
         return props.handleResetScore()
     }
 
-    const cardsRender = () => {
-        return state.map(card => ( 
-            <div key={card.id} onClick={() => {cardClick(card)}}>
+    const handleCardsRender = () => {
+        return cards.map(card => ( 
+            <div key={card.id} onClick={() => {handleCardClick(card)}}>
                 <img src={card.image} alt={card.name}/>
                 <p>{card.name}</p>
             </div>
@@ -101,18 +91,20 @@ export default function Cards(props) {
 
 
   return (
-    <div className='cards'>
-        {cardsRender()}
+    <div className='cards-container'>
+        {handleCardsRender()}
     </div>
   )
 }
 
 
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
+    const cardsCopy = [...array];
+    for (let i = cardsCopy.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        let temp = cardsCopy[i];
+        cardsCopy[i] = cardsCopy[j];
+        cardsCopy[j] = temp;
     }
+    return cardsCopy
 }
