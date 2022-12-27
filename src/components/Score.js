596c297ed2a4bfcec79b2a3fbb5ebc5dbf6bb21f
Marcus <marcusicaromc@gmail.com>
2022-12-27 19:52:55 -0300

    Remove restrict mode and add win condition

--- src/components/Score.js
+++ src/components/Score.js
@@ -4,6 +4,7 @@ export default function Score(props) {
 
   return (
     <div id='score'>
+        {props.bestScore === 10 && alert('You won')}
         <p id='current'><b>Current score: {props.currentScore}</b></p>
         <p id='best'><b>Best score: {props.bestScore}</b></p>
     </div>
